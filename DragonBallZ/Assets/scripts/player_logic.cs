509a6d8c5b7d2240e1dbeaa8b47898dc5b3fedce
Dikshit Kaushal <47450978+dikshitkaushal@users.noreply.github.com>
2020-04-12 01:36:31 +0530

    adding otherplayer using enum and adding animations

--- DragonBallZ/Assets/scripts/player_logic.cs
+++ DragonBallZ/Assets/scripts/player_logic.cs
@@ -10,10 +10,12 @@ public enum playerid
 
 public class player_logic : MonoBehaviour
 {
+    Animator m_animator;
     CharacterController m_charactercontroller;
-    Vector3 m_horizontalmove;
-    Vector3 m_verticalmove;
+    float m_horizontalmove;
+    float m_verticalmove;
     Vector3 m_jump;
+    Vector3 m_totalmove;
     float m_jumpheight = 0.25f;
     float m_gravity = 0.97f;
     float m_horizontalaxis;
@@ -25,6 +27,7 @@ public class player_logic : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        m_animator = GetComponent<Animator>();
         m_charactercontroller = GetComponent<CharacterController>();
     }
 
@@ -33,7 +36,10 @@ public class player_logic : MonoBehaviour
     {
         m_horizontalaxis = Input.GetAxis("Horizontal"+m_playerid);
         m_verticalaxis = Input.GetAxis("Vertical"+m_playerid);
-
+        if(m_animator)
+        {
+            m_animator.SetFloat("movement", Mathf.Max(Mathf.Abs(m_horizontalaxis), Mathf.Abs(m_verticalaxis)));
+        }
         if(Input.GetButtonDown("Jump"+m_playerid) && m_charactercontroller.isGrounded)
         {
             isjumping = true;
@@ -42,21 +48,51 @@ public class player_logic : MonoBehaviour
     private void FixedUpdate()
     {
         //jumping logic
-        if(isjumping)
+        if (isjumping)
         {
             isjumping = false;
             m_jump.y = m_jumpheight;
         }
         m_jump.y -= m_gravity * Time.deltaTime;
-       
+
+
+
+        /* if (!m_charactercontroller.isGrounded)
+         {
+             if (m_jump.y > 0)
+             {
+                 m_jump.y -= m_gravity * Time.deltaTime;
+             }
+             else
+             {
+                 m_jump.y -= m_gravity * Time.deltaTime * 2;
+             }
+
+         }
+
+         if (isjumping)
+         {
+             m_jump.y = m_jumpheight;
+             isjumping = false; ;
+         }
+         Debug.Log(m_jump.y);
+ */
         //moving logic
 
-        m_horizontalmove = transform.right * m_horizontalaxis * speed * Time.deltaTime;
-        m_verticalmove = transform.forward * m_verticalaxis * speed * Time.deltaTime;
-        if(m_charactercontroller)
+        m_horizontalmove =  m_horizontalaxis * speed * Time.deltaTime;
+        m_verticalmove = m_verticalaxis * speed * Time.deltaTime;
+        m_totalmove = new Vector3(m_horizontalmove, 0, m_verticalmove);
+        if (m_totalmove!=Vector3.zero)
         {
-            m_charactercontroller.Move(m_horizontalmove + m_verticalmove + m_jump);
+            transform.forward = m_totalmove.normalized;
+            
         }
+        if (m_charactercontroller)
+        {
+            m_charactercontroller.Move(m_totalmove + m_jump);
+        }
+
+
         if (m_charactercontroller.isGrounded)
         {
             m_jump.y = 0;
